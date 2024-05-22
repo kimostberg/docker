17179afc2282cd3bdc14b5dda8eb26a4e26fe7d9
KimOstberg <mail@kimostberg.it>
2024-05-22 18:54:47 +0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -18,7 +18,7 @@ function check_internet() {
 check_internet
 
 # Add Docker's official GPG key:
-sudo apt-get update
+sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
