525b550a14b5bf2bc8e934391741c08ba677d60f
KimOstberg <mail@kimostberg.it>
2024-05-22 16:22:41 +0300

    Create create_user.sh

---
+++ create_user.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Please enter a username:"
+read username
+
+if id -u "$username" >/dev/null 2>&1; then
+  echo "User $username already exists, exiting..."
+  exit 1
+fi
+
+useradd -m -s /bin/bash "$username"
+
+if [ $? -eq 0 ]; then
+  echo "Adding user $username to sudo group..."
+  usermod -aG sudo "$username"
+else
+  echo "Failed to create user $username, exiting..."
+  exit 1
+fi
+
+echo "User $username has been created and added to the sudo group."
