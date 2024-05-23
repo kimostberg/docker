1ec90e31a3d2eb6036d3b8b50a1ded7fb844c035
KimOstberg <mail@kimostberg.it>
2024-05-23 10:22:17 +0300

    Create install.sh

---
+++ containers/netbird-client/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p /docker/netbird-client
+cd /docker/netbird-client
+wget https://raw.githubusercontent.com/kimostberg/docker/main/containers/netbird-client/compose.yml
+
+read -p "Enter client name: " CLIENT_NAME
+read -p "Enter setup key: " NB_SETUP_KEY
+
+echo "CLIENT_NAME=$CLIENT_NAME" >> ~/.env
+echo "NB_SETUP_KEY=$NB_SETUP_KEY" >> ~/.env
