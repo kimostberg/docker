e71141989792e3eae464ffca77c3cb30c2564daf
KimOstberg <mail@kimostberg.it>
2024-05-23 10:59:50 +0300

    added line to remove possible exixting .env

--- containers/netbird-client/install.sh
+++ containers/netbird-client/install.sh
@@ -2,14 +2,14 @@
 mkdir -p /docker/netbird-client
 cd /docker/netbird-client
 wget https://raw.githubusercontent.com/kimostberg/docker/main/containers/netbird-client/compose.yml
-
+rm .env
 read -p "Enter client name: " CLIENT_NAME
 read -p "Enter setup key: " SETUP_KEY
 read -p "Enter management URL: " MANAGEMENT_URL
 
-echo "CLIENT_NAME=$CLIENT_NAME" >> ~/.env
-echo "SETUP_KEY=$SETUP_KEY" >> ~/.env
-echo "MANAGEMENT_URL=$MANAGEMENT_URL" >> ~/.env
+echo "CLIENT_NAME=$CLIENT_NAME" > .env
+echo "SETUP_KEY=$SETUP_KEY" >> .env
+echo "MANAGEMENT_URL=$MANAGEMENT_URL" >> .env
 
 docker compose pull
 docker compose up -d
