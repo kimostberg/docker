d963563b80e8fd0e2f2851839d64b44dca910d8c
KimOstberg <mail@kimostberg.it>
2024-05-23 10:34:56 +0300

    Added management url

--- containers/netbird-client/install.sh
+++ containers/netbird-client/install.sh
@@ -4,10 +4,12 @@ cd /docker/netbird-client
 wget https://raw.githubusercontent.com/kimostberg/docker/main/containers/netbird-client/compose.yml
 
 read -p "Enter client name: " CLIENT_NAME
-read -p "Enter setup key: " NB_SETUP_KEY
+read -p "Enter setup key: " SETUP_KEY
+read -p "Enter management URL: " MANAGEMENT_URL
 
 echo "CLIENT_NAME=$CLIENT_NAME" >> ~/.env
-echo "NB_SETUP_KEY=$NB_SETUP_KEY" >> ~/.env
+echo "SETUP_KEY=$SETUP_KEY" >> ~/.env
+echo "MANAGEMENT_URL=$MANAGEMENT_URL" >> ~/.env
 
 docker compose pull
 docker compose up -d
